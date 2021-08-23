19cbd6d894f97e044dcd6919b3fbcf1870a43864
guilhermematos13 <guilhermemoliveira1998@gmail.com>
2021-08-23 18:23:16 -0300

    first commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,26 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { CalculadoraModule} from './calculadora'
+import { CalculadoraService} from './calculadora/service';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+      imports: [
+        CalculadoraModule
+      ],
+      providers: [
+        CalculadoraService
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
